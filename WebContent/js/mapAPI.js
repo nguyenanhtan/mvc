fe8b3f7eca9f4eae9ca7f22c64ddbbedc51580bd
nguyenanhtan <tanna.ithust@gmail.com>
2014-03-12 15:46:57 +0700

    12/3

--- WebContent/js/mapAPI.js
+++ WebContent/js/mapAPI.js
@@ -6,12 +6,13 @@ var icon_deliver = "./img/Map-Marker-Ball-Pink-icon.png";
 var icon_marker = "";//icon_pickup;
 var btn_pickup = document.getElementById("btn-pickup");
 var btn_deliver = document.getElementById("btn-deliver");
-
+var pos_pickup;
+var pos_deliver;
+var ele_focus = null;
 var directionsService = new google.maps.DirectionsService();
 	
 function initialize() 
-{
-	
+{	
 	directionsDisplay = new google.maps.DirectionsRenderer();
     var mapOptions = {
     center: new google.maps.LatLng(21.006738,105.84138),
@@ -23,47 +24,72 @@ function initialize()
     google.maps.event.addListener(map, 'click', function(event) {
     	  placeMarker(event.latLng);
       });
+    
   }
-  function placeMarker(location) {
-  	  var marker = new google.maps.Marker({
-  	      position: location,
-  	      map: map,
-  	      draggable:true,
-  	      title:"Drag me!",
-  	      icon: icon_marker
-  	  });
-  	  markers.push(marker); 
-    }
-      function calcRoute(mStart,mEnd) {
-    	  if(mStart > markers.length || mEnd > markers.length)
-    		  {
-    		  	alert("mStart > markers.length || mEnd > markers.length");
-    		  }
-    	  var start = markers[mStart].getPosition();
-    	  var end = markers[mEnd].getPosition();
-    	  var request = {
-    	      origin:start,
-    	      destination:end,
-    	      travelMode: google.maps.TravelMode.DRIVING,
-    	      optimizeWaypoints: true
-    	  };
-    	  directionsService.route(request, function(response, status) {
-    		  //alert("Status: "+status);
-    	    if (status == google.maps.DirectionsStatus.OK) {
-    	      directionsDisplay.setDirections(response);
-    	    }
-    	  });
-    	}
+  function placeMarker(location) {	 
+	  //alert( ele_focus.id+"/"+btn_deliver+"/"+btn_pickup);
+	  //alert(pos_deliver);
+	  //pos_deliver.value = "OK";
+	  if(btn_deliver == ele_focus)
+	  {
+		  icon_marker = icon_deliver;
+		  pos_deliver.value = location;
+	  }
+	  else if(btn_pickup == ele_focus)
+	  {
+		  icon_marker = icon_pickup;
+		  pos_pickup.value = location;
+	  }
+	  if(btn_deliver == ele_focus || btn_pickup == ele_focus)
+	  {		 
+	  	  var marker = new google.maps.Marker({
+	  	      position: location,
+	  	      map: map,
+	  	      draggable:true,
+	  	      title:"Kéo để chuyển vị trí!",
+	  	      icon: icon_marker
+	  	  });
+	  	  
+		  var infowindow = new google.maps.InfoWindow({
+		      content: "Heheheheheheh"
+		  });
+	  	  google.maps.event.addListener(marker, 'click', function() {
+	  	       infowindow.open(map,marker);
+	  	  });
 
-      google.maps.event.addDomListener(window, 'load', initialize);
-      
-$(document).ready(function(){
-	$("#btn-pickup").focus(function(){
-		icon_marker = icon_pickup;
-		
-	});
-	$("#btn-deliver").focus(function(){
-		icon_marker = icon_deliver;
-		
-	});
+	  	  markers.push(marker);
+	  }	  
+    }
+  function calcRoute(mStart,mEnd) {
+	  if(mStart > markers.length || mEnd > markers.length)
+	  {
+	  	alert("mStart > markers.length || mEnd > markers.length");
+	  }
+	  var start = markers[mStart].getPosition();
+	  var end = markers[mEnd].getPosition();
+	  var request = {
+	      origin:start,
+	      destination:end,
+	      travelMode: google.maps.TravelMode.DRIVING,
+	      optimizeWaypoints: true
+	  };
+	  directionsService.route(request, function(response, status) {
+		  //alert("Status: "+status);
+	    if (status == google.maps.DirectionsStatus.OK) {
+	      directionsDisplay.setDirections(response);
+	    }
+	  });
+	}
+
+ google.maps.event.addDomListener(window, 'load', initialize);  
+ window.onload = function(){
+	 btn_pickup = document.getElementById("btn-pickup");
+	 btn_deliver = document.getElementById("btn-deliver");
+	 pos_pickup = document.getElementById("pos-pickup");
+	 pos_deliver = document.getElementById("pos-deliver");
+	 //alert("btn_pickup: "+btn_pickup);
+ };
+$(document).ready(function(){	
+	$("*").focus(function(){ele_focus = this;});
+	$("*").blur(function(){ele_focus = null;});
 });
--- WebContent/login-success.jsp
+++ WebContent/login-success.jsp
@@ -17,39 +17,42 @@
   <body>
  
 <p>You are successfully logged in!</p>
+
 <div id="map-canvas"></div>
+<div id="right">
 <input type="button" id="route" value="Route" onclick="calcRoute(1,2)"/>     
 <p id="console"></p>
 <input type="button" id="btn-add" class="btn-opt-require"/>
 <input type="button" id="btn-remove" class="btn-opt-require"/>
 
 <form id="form-opt-require">
-<div>
-<input type="button" id="btn-pickup" class="btn-opt-require"/>
-<input type="text" value="position pickup" class="opacity-60"/>
+<div id="box-require">
+	<div>
+		<input type="button" id="btn-pickup" class="btn-opt-require"/>
+		<input type="text" value="position pickup" class="opacity-60" id="pos-pickup"/>
+	</div>
+	<div>
+		<input type="button" id="btn-deliver" class="btn-opt-require"/>
+		<input type="text" value="position deliver" class="opacity-60" id="pos-deliver"/>
+	
+	</div>
+	<div>
+		<p>Weight:</p><input class="ipt-text" type="number" value=""/>
+	</div>
+	<div>
+		<p>Ep:</p><input class="ipt-text" type="time" value=""/>
+	</div>
+	<div>
+		<p>Lp:</p><input class="ipt-text" type="time" value=""/>
+	</div>
+	<div>
+		<p>Ed:</p><input class="ipt-text" type="time" value=""/>
+	</div>
+	<div>
+		<p>Lp:</p><input class="ipt-text" type="time" value=""/>
+	</div>
 </div>
-<div>
-<input type="button" id="btn-deliver" class="btn-opt-require"/>
-<input type="text" value="position deliver" class="opacity-60"/>
-
-</div>
-<div>
-<p>Weight:</p><input class="ipt-text" type="number" value=""/>
-</div>
-<div>
-<p>Ep:</p><input class="ipt-text" type="time" value=""/>
-</div>
-<div>
-<p>Lp:</p><input class="ipt-text" type="time" value=""/>
-</div>
-<div>
-<p>Ed:</p><input class="ipt-text" type="time" value=""/>
-</div>
-<div>
-<p>Lp:</p><input class="ipt-text" type="time" value=""/>
-</div>
-
 </form>
-
+</div>
   </body>
 </html>
